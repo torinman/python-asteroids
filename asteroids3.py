1ccf862cae5f697fcd2736c27242e06d3b76b573
Torin Stephens <torin@theblorenge.co.uk>
2021-03-15 12:57:39 +1300

    test1

--- asteroids3.py
+++ asteroids3.py
@@ -21,6 +21,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see  <https://www.gnu.org/licenses/>.
 # 
+#    Test 1
 from __future__ import division
 import math
 import random
